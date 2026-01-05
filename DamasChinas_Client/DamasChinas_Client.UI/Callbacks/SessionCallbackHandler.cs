2ec799ae5d632b8afbb0a3d49011342447c6f0f6
Ivan <ir811362@gmail.com>
2026-01-04 22:00:15 -0600

    Reducción de deuda

--- DamasChinas_Client/DamasChinas_Client.UI/Callbacks/SessionCallbackHandler.cs
+++ DamasChinas_Client/DamasChinas_Client.UI/Callbacks/SessionCallbackHandler.cs
@@ -14,17 +14,16 @@ namespace DamasChinas_Client.UI.Callbacks
         public static event Action SessionExpiredEvent;
         public static event Action<string> PlayerInGameEvent;
         public static event Action<string> PlayerLeftGameEvent;
-
         public static event Action<BanInfoDto> BanStatusUpdatedEvent;
 
-        public void PlayerConnected(string username)
+        public void PlayerConnected(string nickname)
         {
-            PlayerConnectedEvent?.Invoke(username);
+            PlayerConnectedEvent?.Invoke(nickname);
         }
 
-        public void PlayerDisconnected(string username)
+        public void PlayerDisconnected(string nickname)
         {
-            PlayerDisconnectedEvent?.Invoke(username);
+            PlayerDisconnectedEvent?.Invoke(nickname);
         }
 
         public void SessionExpired()
@@ -32,14 +31,14 @@ namespace DamasChinas_Client.UI.Callbacks
             SessionExpiredEvent?.Invoke();
         }
 
-        public void PlayerInGame(string username)
+        public void PlayerInGame(string nickname)
         {
-            PlayerInGameEvent?.Invoke(username);
+            PlayerInGameEvent?.Invoke(nickname);
         }
 
-        public void PlayerLeftGame(string username)
+        public void PlayerLeftGame(string nickname)
         {
-            PlayerLeftGameEvent?.Invoke(username);
+            PlayerLeftGameEvent?.Invoke(nickname);
         }
 
         public void OnForcedLogout(string code)
@@ -82,13 +81,11 @@ namespace DamasChinas_Client.UI.Callbacks
                 return MessageKeys.DatabaseUnavailable;
             }
 
-         
             if (code.StartsWith("msg_", StringComparison.OrdinalIgnoreCase))
             {
                 return code;
             }
 
-      
             if (code.Equals("DatabaseUnavailable", StringComparison.OrdinalIgnoreCase))
             {
                 return MessageKeys.DatabaseUnavailable;
@@ -104,13 +101,8 @@ namespace DamasChinas_Client.UI.Callbacks
                 return MessageKeys.SessionExpired;
             }
 
-     
             return MessageKeys.UnknownError;
         }
-
-
-
-
 
         public void OnBanStatusUpdated(BanInfoDto banInfo)
         {
@@ -147,7 +139,6 @@ namespace DamasChinas_Client.UI.Callbacks
 
                     PendingBanNotificationStore.Clear();
 
-             
                     try
                     {
                         ClientSession.ClearForced();
